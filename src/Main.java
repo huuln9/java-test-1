813794a02ac853939c285bc229af299909626ef9
huuln9 <huuln9@gmail.com>
2024-10-05 09:22:53 +0700

    revert

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        LopHoc lopHoc = new LopHoc();
-        lopHoc.nhap();
-        lopHoc.xuatTheoDiem();
-    }
-    
-    
-}
