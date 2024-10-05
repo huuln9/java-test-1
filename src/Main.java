d549abf2d135580db14a5f09fc493c9bb6ebba0d
huuln9 <huuln9@gmail.com>
2024-10-05 09:23:33 +0700

    update

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LopHoc lopHoc = new LopHoc();
+        lopHoc.nhap();
+        lopHoc.xuatTheoDiem();
+    }
+    
+    
+}
